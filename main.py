cd2a503b6a5a2b9ec357e2f18ade679772341eb8
Suyash L <suyashblal@gmail.com>
2021-03-24 21:40:47 +0000

    NUmber Genarator

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
  # Write a program that uses a loop fill an empty list with 10 numbers and prints the list to the console. The numbers can be random or simply put in the numbers 0-9 or 1-10 in ascending order.
 
-
-list2 = ('1','2','3','4','5','6','7','8','9','10')
+import random
+list2 = []
+for i in range (10):
+  list2.append(random.randint(0,99))
 print(list2)
-for numbers in range(10):
-  list.append(random.randint(1,10))
-  
+list2.sort()
+print(list2)
